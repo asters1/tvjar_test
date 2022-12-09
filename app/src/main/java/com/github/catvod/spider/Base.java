1fdc6216b5d8126827459ffa6eec7a866736186c
asters1 <gj744559140@163.com>
2022-12-09 01:57:15 +0000

    完善结构

--- app/src/main/java/com/github/catvod/spider/Base.java
+++ app/src/main/java/com/github/catvod/spider/Base.java
@@ -6,26 +6,52 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.github.catvod.crawler.Spider;
+import com.github.catvod.crawler.SpiderDebug;
 
 public class Base extends Spider {
 
     public String homeContent(boolean filter) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
         return "";
     }
 
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
         return "";
     }
 
     public String detailContent(List<String> ids) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
         return "";
     }
 
     public String searchContent(String key, boolean quick) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
         return "";
     }
 
     public String playerContent(String flag, String id, List<String> vipFlags) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
         return "";
     }
 }
