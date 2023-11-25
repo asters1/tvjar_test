49c9e1a1471a6983faea6dbc2d73b69e5f257e68
asters1 <gj744559140@163.com>
2023-11-25 02:25:27 +0000

    更新android

--- app/src/main/java/android/os/Build.java
+++ app/src/main/java/android/os/Build.java
@@ -2,6 +2,7 @@ package android.os;
 
 public class Build {
   public static String BRAND="Redmi";
+  public static String MODEL="M2012K11AC";
   public static class VERSION{
     public static String RELEASE="14";
 
