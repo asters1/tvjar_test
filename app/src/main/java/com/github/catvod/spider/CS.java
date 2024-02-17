340d4a46a7931fb1b51e00574ae940cffabb68e3
asters1 <gj744559140@163.com>
2024-02-17 07:51:52 +0000

    CS.java

--- app/src/main/java/com/github/catvod/spider/CS.java
+++ app/src/main/java/com/github/catvod/spider/CS.java
@@ -20,74 +20,77 @@ import com.github.catvod.crawler.SpiderDebug;
 
 public class CS extends Spider {
 
-    private static final String siteUrl = "";
-    //ext为外部给的字符串
-    public String ext = "";
+  private static final String siteUrl = "";
+  //ext为外部给的字符串
+  public String ext = "";
 
-    public void init(Context context,String ext) {
-        super.init(context,ext);
-        this.ext=ext;
+  public void init(Context context,String ext) {
+    super.init(context,ext);
+    this.ext=ext;
+  }
+
+  public String homeContent(boolean filter) {
+    try {
+      // System.out.println(ext);
+
+    } catch (Exception e) {
+      SpiderDebug.log(e);
     }
+    return "";
+  }
 
-    public String homeContent(boolean filter) {
-        try {
-          // System.out.println(ext);
+  public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
+    try {
 
-        } catch (Exception e) {
-            SpiderDebug.log(e);
-        }
-        return "";
+    } catch (Exception e) {
+      SpiderDebug.log(e);
     }
+    return "";
+  }
 
-    public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
-        try {
+  public String detailContent(List<String> ids) {
+    try {
 
-        } catch (Exception e) {
-            SpiderDebug.log(e);
-        }
-        return "";
+    } catch (Exception e) {
+      SpiderDebug.log(e);
     }
+    return "";
+  }
 
-    public String detailContent(List<String> ids) {
-        try {
+  public String searchContent(String key, boolean quick) {
+    try {
 
-        } catch (Exception e) {
-            SpiderDebug.log(e);
-        }
-        return "";
+    } catch (Exception e) {
+      SpiderDebug.log(e);
     }
+    return "";
+  }
 
-    public String searchContent(String key, boolean quick) {
-        try {
+  public String playerContent(String flag, String id, List<String> vipFlags) {
+    try {
 
-        } catch (Exception e) {
-            SpiderDebug.log(e);
-        }
-        return "";
+    } catch (Exception e) {
+      SpiderDebug.log(e);
     }
+    return "";
+  }
 
-    public String playerContent(String flag, String id, List<String> vipFlags) {
-        try {
+  // ====================
+  void printLog(String key, String value) {
+    try {
 
-        } catch (Exception e) {
-            SpiderDebug.log(e);
-        }
-        return "";
+      String str = key + "=" + value;
+      String str1 = "http://localhost:8080/?" + str;
+      String res = OkHttpUtil.string(str1, null);
+      System.out.println(res);
+    } catch (Exception e) {
     }
-
-    // ====================
-    void printLog(String key, String value) {
-        try {
-
-            String str = key + "=" + value;
-            String str1 = "http://localhost:8080/?" + str;
-            String res = OkHttpUtil.string(str1, null);
-            System.out.println(res);
-        } catch (Exception e) {
-        }
-    }
-    // ====================
+  }
+  // ====================
   public  boolean validateActCode(String str){
+    if (str.equals("")){
+      return false;
+    }
     return true;
   }
 }
