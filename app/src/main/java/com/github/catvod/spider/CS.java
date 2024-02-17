2c8fc195a6848103c661307ddd82361688d91388
asters1 <gj744559140@163.com>
2024-02-17 07:32:42 +0000

    CS.java

---
+++ app/src/main/java/com/github/catvod/spider/CS.java
@@ -0,0 +1,93 @@
+package com.github.catvod.spider;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Base64;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.github.catvod.utils.okhttp.OkHttpUtil;
+
+import com.github.catvod.crawler.Spider;
+import com.github.catvod.crawler.SpiderDebug;
+
+public class CS extends Spider {
+
+    private static final String siteUrl = "";
+    //ext为外部给的字符串
+    public String ext = "";
+
+    public void init(Context context,String ext) {
+        super.init(context,ext);
+        this.ext=ext;
+    }
+
+    public String homeContent(boolean filter) {
+        try {
+          // System.out.println(ext);
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
+        return "";
+    }
+
+    public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
+        return "";
+    }
+
+    public String detailContent(List<String> ids) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
+        return "";
+    }
+
+    public String searchContent(String key, boolean quick) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
+        return "";
+    }
+
+    public String playerContent(String flag, String id, List<String> vipFlags) {
+        try {
+
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
+        return "";
+    }
+
+    // ====================
+    void printLog(String key, String value) {
+        try {
+
+            String str = key + "=" + value;
+            String str1 = "http://localhost:8080/?" + str;
+            String res = OkHttpUtil.string(str1, null);
+            System.out.println(res);
+        } catch (Exception e) {
+        }
+    }
+    // ====================
+  public  boolean validateActCode(String str){
+    return true;
+  }
+}
